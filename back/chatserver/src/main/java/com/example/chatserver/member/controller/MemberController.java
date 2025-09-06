9036999895c757b4521a170926ef86413a13c3e2
happ-in <gpqls304@naver.com>
2025-09-06 18:44:00 +0900

    Feat: jwt token 인증

--- back/chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
+++ back/chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
@@ -1,19 +1,18 @@
 package com.example.chatserver.member.controller;
 
 import com.example.chatserver.common.auth.JwtTokenProvider;
+import com.example.chatserver.member.dto.MemberListResDto;
 import com.example.chatserver.member.dto.MemberLoginReqDto;
 import com.example.chatserver.member.dto.MemberSaveReqDto;
 import com.example.chatserver.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.chatserver.member.domain.Member;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -44,5 +43,11 @@ public class MemberController {
         loginInfo.put("token", jwtToken);
 
         return new ResponseEntity<>(loginInfo, HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> memberList() {
+        List<MemberListResDto> dtos = memberService.findAll();
+        return new ResponseEntity<>(dtos, HttpStatus.OK);
     }
 }
